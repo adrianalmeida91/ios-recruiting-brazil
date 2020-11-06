d5a3f2fad9f4d2cd9b9a6e7b66d58e50885c6eac
Adrian Almeida <drialme@zup002107-nb.itau.corp.ihf>
2020-11-06 16:47:16 -0300

    update gitignore

---
+++ Movs/MovsTests/Sources/Components/GridGallery/GridGalleryItemViewTests.swift
@@ -0,0 +1,9 @@
+//
+//  GridGalleryItemViewTests.swift
+//  MovsTests
+//
+//  Created by Adrian Almeida on 06/11/20.
+//  Copyright © 2020 Adrian Almeida. All rights reserved.
+//
+
+import Foundation
